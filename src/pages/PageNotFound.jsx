a9d1f59124e296598fa8eeee63fbf10b52146191
VladITGod <vorobiov.v90@gmail.com>
2023-05-02 16:31:21 +0300

    subtitle margin changed

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -12,8 +12,8 @@ const PageNotFound = () => {
         <Container>
             <div className="outer-wrapper text-center mt-5 m-auto w-50 ">
                 <h1 className='mb-3 fw-bold'>Oops!</h1>
-                <h5 className="fw-normal">Sorry, the requested page does not exist.</h5>
-                <Button className="btn mt-5"
+                <h6 className="fw-normal mb-4">Sorry, the requested page does not exist.</h6>
+                <Button className="btn"
                         color="green"
                         appearance="primary"
                         onClick={navigateHome}>Home
